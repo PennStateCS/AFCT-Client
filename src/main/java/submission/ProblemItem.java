2022101a01690e4f8c7592322d9f018a6bb0cebf
Edwin Kimsal <kimsal.edwin@gmail.com>
2025-11-27 21:07:01 -0500

    Restructured code for better structure and better performance

---
+++ src/main/java/submission/ProblemItem.java
@@ -0,0 +1,23 @@
+package submission;
+
+// Class used as items for problem drop-down menu
+public class ProblemItem {
+    String id;
+    String title;
+
+    ProblemItem(String id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+    // Setters
+    public void setId(String id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+
+    // Getters
+    public String getId() { return this.id; }
+    public String getTitle() { return this.title; }
+
+    // Helpers
+    public String toString() { return this.title; }
+}
