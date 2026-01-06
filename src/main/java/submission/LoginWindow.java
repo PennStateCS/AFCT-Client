16be4a95d5877f6a1486ab5d43c403e32af79003
jb4411 <jb4411@rit.edu>
2026-01-05 21:43:45 -0500

    Working on revamping submission GUI

---
+++ src/main/java/submission/LoginWindow.java
@@ -0,0 +1,13 @@
+package submission;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginWindow extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
