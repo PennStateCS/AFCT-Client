7d8524f4eee574106f8dfe01df3ff9ccfac139ab
jb4411 <jb4411@rit.edu>
2026-02-21 14:31:17 -0500

    v1.6.5 - Working on Certificate handling, these changes may end up bing moved to v1.6.6 or later...

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -48,6 +48,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import submission.CertificatePopup;
 
 import static gui.Globals.*;
 
@@ -145,6 +146,9 @@ public class Main {
 		}
         Universe.curProfile.loadPreferences();
 		updater.updatePopup.showOnLoad();
+
+        // TODO: remove after testing
+        new CertificatePopup(null);
 
 		if (args.length >= 2) {
 			print(Arrays.toString(args));
