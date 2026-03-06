d70678c7e0dad99a64ee665b1bc6e87c0da2eac5
TeddyFitzPatrick <tfitz200@gmail.com>
2026-03-06 13:46:45 -0500

    add GNFA XML transducer

--- src/main/java/file/xml/TransducerFactory.java
+++ src/main/java/file/xml/TransducerFactory.java
@@ -194,6 +194,7 @@ public class TransducerFactory {
 	static {
 		typeToTransducer = new HashMap<>();
 		classToTransducer = new HashMap<>();
+		add(null, automata.gnfa.GNFA.class, new FSATransducer());
 		add(null, automata.fsa.FiniteStateAutomaton.class, new FSATransducer());
 		add(null, automata.pda.PushdownAutomaton.class, new PDATransducer());
 		add(null, automata.turing.TuringMachine.class, new TMTransducer());
