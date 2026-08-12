5a1a51ec1f9ebedd4cc513ca781aebea1ac23338
jb4411 <jb4411@rit.edu>
2026-08-12 16:48:32 -0400

    v1.6.9 - Split the VERSION string in AboutBox into JFLAP_VERSION and AFCT_VERSION. This fixes the FILE_CREATED_WITH_STRING in GLobals, so that it now reports the correct JFLAP version the software was based on.
    In Globals, re-implemented resolveCurrentVersion() which appers to have been lost when the SSL-Issue branch was rebased.

--- src/main/java/gui/AboutBox.java
+++ src/main/java/gui/AboutBox.java
@@ -123,7 +123,8 @@ public class AboutBox extends JWindow {
 	private static Image IMAGE = null;
 
 	/** The version string. */
-	public static final String VERSION = "1.6.8";
+	public static final String JFLAP_VERSION = "7.1";
+	public static final String AFCT_VERSION = "1.6.9";
 
 	/**
 	 * Some simple test code for the about box.
