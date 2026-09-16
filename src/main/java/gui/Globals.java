9eb172e8e6a26c795b89ed51dd7616c713a6c6e9
jb4411 <jb4411@rit.edu>
2026-09-16 16:40:44 -0400

    v1.6.9 - Removed unused implementationVersion() function.

--- src/main/java/gui/Globals.java
+++ src/main/java/gui/Globals.java
@@ -48,11 +48,6 @@ public class Globals {
     }
     public static final String JUST_NAME = "AFCT ";
     public final static String APP_NAME = (JUST_NAME + currentVersion).trim();
-
-    private static String implementationVersion() {
-        String v = Globals.class.getPackage().getImplementationVersion();
-        return v == null ? "" : "v" + v;
-    }
     public static final String APP_URL = "https://www.cs.rit.edu/~afct";
     public static final String LATEST_RELEASE_PATH = "/client/";
     public static String JAR_PATH = "TODO";
